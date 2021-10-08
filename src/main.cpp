466a952f1d89a5866324eb9af208bd72d4da853e
geoff-driskell <42808408+geoff-driskell@users.noreply.github.com>
2021-10-08 10:22:55 -0400

    Created game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,18 @@
-#include "../include/SDL2/SDL.h"
-#include <iostream>
+#include "game.hpp"
 
-using namespace std;
+Game *game = nullptr;
 
-int main(){
-    int width=800, height=600;
-    SDL_Window *window=SDL_CreateWindow("Window", SDL_WINDOWPOS_CENTERED, 
-    SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_RESIZABLE);
+int main(int argc, const char * argv[])
+{
+    game = new Game();
+    game->init("GeoffGame", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, true);
 
-    if(window==NULL)
-        cout << "There was an error while initializing the window" << endl << SDL_GetError() << endl;
-
-    SDL_Event event;
-    bool running=true;
-
-    while(running){
-        while(SDL_PollEvent(&event)){
-            if(event.type==SDL_QUIT){
-                running=false;
-                break;
-            }
-        }
-        SDL_GetWindowSize(window, &width, &height);
-
-        SDL_Surface *surface=SDL_GetWindowSurface(window);
-        Uint32 green=SDL_MapRGB(surface->format, 0,255,0);
-        SDL_FillRect(surface, NULL, green);
-        SDL_UpdateWindowSurface(window);
+    while (game->running()) 
+    {
+        game->handleEvents();
+        game->update();
+        game->render();
     }
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    game->clean();
 }
