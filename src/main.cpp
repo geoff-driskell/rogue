83924caad1a546e50db9652ee96b103867db9cee
Geoffrey Driskell <geoff.driskell@gmail.com>
2021-10-11 09:02:55 -0400

    boosted frame limiting comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "game.hpp"
 
 Game *game = nullptr;
+bool firstFrame = true;
 
 int main(int argc, const char * argv[])
 {
     const int fps = 60;
     const int framePlanned = 1000 / fps;
+    int frameDifference;
 
     uint32_t frameStart;
     int frameActual;
@@ -23,11 +25,28 @@ int main(int argc, const char * argv[])
         game->update();
         game->render();
 
+        if (firstFrame)
+        {
+            SDL_Delay(17);
+            firstFrame = false;
+        }
+
         frameActual = SDL_GetTicks() - frameStart;
 
-        if (frameActual < framePlanned)
+        frameDifference = framePlanned - frameActual;
+
+        if (frameDifference > 0)
         {
-            SDL_Delay(framePlanned - frameActual);
+            SDL_Delay(frameDifference);
+            std::cout << "Game is ahead, delaying by " << frameDifference << " milliseconds." << std::endl;
+        }
+        else if (frameDifference == 0)
+        {
+            std::cout << "Game is on time, continuing..." << std::endl;
+        }
+        else
+        {
+            std::cout << "Game is behind by " << abs(frameDifference) << " milliseconds. Hopefully we catch up." << std::endl;
         }
     }
 
