09989f2996b65ec87fc6402c4fdb5595cdc03f27
Geoffrey Driskell <geoff.driskell@gmail.com>
2021-10-21 08:52:03 -0400

    Created a sprite component for the player

---
+++ src/ECS/spriteComponent.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "components.hpp"
+#include "../../include/SDL.h"
+#include "../textureManager.hpp"
+
+class SpriteComponent : public Component
+{
+private:
+    StateComponent *state;
+    SDL_Texture *texture;
+    SDL_Rect srcRect, destRect;
+
+public:
+    SpriteComponent() = default;
+    SpriteComponent(const char* path)
+    {
+        setTexture(path);
+    }
+    
+    void setTexture(const char* path)
+    {
+        texture = TextureManager::LoadTexture(path);
+    }
+
+    void init() override
+    {
+        state = &entity->getComponent<StateComponent>();
+        
+        srcRect.x = 0;
+        srcRect.y = 64;
+        srcRect.w = srcRect.h = 32;
+        destRect.w = destRect.h = 32;
+    }
+
+    void update() override
+    {
+        destRect.x = state->x();
+        destRect.y = state->y();
+    }
+
+    void draw() override
+    {
+        TextureManager::Draw(texture, srcRect, destRect);
+    }
+
+    void changeFrame(int frame)
+    {
+        srcRect.x = frame * srcRect.w;
+    }
+};
