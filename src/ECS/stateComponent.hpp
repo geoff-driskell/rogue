596db65fbc908bec22bd8703f2ee8d496fd9e0f9
geoff-driskell <42808408+geoff-driskell@users.noreply.github.com>
2021-10-21 11:25:58 -0400

    Added concept of velocity to stateComponent

--- src/ECS/stateComponent.hpp
+++ src/ECS/stateComponent.hpp
@@ -7,6 +7,11 @@ class StateComponent : public Component
 
 public:
     Vector2D position;
+    Vector2D velocity;
+
+    int speed = 3;
+
+
 
     StateComponent()
     {
@@ -20,8 +25,15 @@ public:
         position.y = y;
     }
 
+    void init() override
+    {
+        velocity.x = 0;
+        velocity.y = 0;
+    }
+
     void update() override
     {
-        
+        position.x += velocity.x * speed;
+        position.y += velocity.y * speed;
     }
 };
