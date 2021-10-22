de7832fdcf84407d67166ac9d29abcefaf757b11
Geoffrey Driskell <geoff.driskell@gmail.com>
2021-10-21 21:28:54 -0400

    Added constructor to take in tag

--- src/ECS/ColliderComponent.hpp
+++ src/ECS/ColliderComponent.hpp
@@ -6,10 +6,16 @@
 
 class ColliderComponent : public Component
 {
+public:
     SDL_Rect collider;
     std::string tag;
 
     StateComponent* state;
+
+    ColliderComponent(std::string t)
+    {
+        tag = t;
+    }
 
     void init() override
     {
@@ -18,5 +24,13 @@ class ColliderComponent : public Component
             entity->addComponent<StateComponent>();
         }
         state = &entity->getComponent<StateComponent>();
+    }
+
+    void update() override
+    {
+        collider.x = static_cast<int>(state->position.x);
+        collider.y = static_cast<int>(state->position.y);
+        collider.w = state->width * state->scale;
+        collider.h = state->height * state->scale;
     }
 };
