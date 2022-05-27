d901a4f31aac38bb07c2b221f4268a49ac169646
Geoff Driskell <geoff.driskell@gmail.com>
2022-05-27 10:02:03 -0400

    Updated loadMap() signature and removed unessary functions and variables

--- src/map.hpp
+++ src/map.hpp
@@ -8,19 +8,11 @@ public:
     Map();
     ~Map();
 
-    void loadMap(int arr[20][25]);
-    void drawMap();
+    static void loadMap(std::string path, int sizeX, int sizeY);
+
 
 
 private:
 
-    SDL_Rect src, dest;
-
-    SDL_Texture* dirt;
-    SDL_Texture* grass;
-    SDL_Texture* wall;
-    SDL_Texture* water;
-
-    int map[20][25];
 
 };
