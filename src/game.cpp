93c7126c427f706d22e38dd3e77d4b9169b7f110
Geoff Driskell <geoff.driskell@gmail.com>
2022-05-27 10:12:09 -0400

    Removed unecessary logging

--- src/game.cpp
+++ src/game.cpp
@@ -41,20 +41,12 @@ void Game::init(const char *title, int xpos, int ypos, int width, int height, bo
 
     if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
     {
-        std::cout << "Subsystems initialized!..." << std::endl;
-
         window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-        if (window)
-        {
-            std::cout << "Window has been created." << std::endl;
-        }
-
         Game::renderer = SDL_CreateRenderer(window, -1, 0);
 
         if (renderer)
         {
             SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-            std::cout << "Renderer has been created!" << std::endl;
         }
 
         isRunning = true;
