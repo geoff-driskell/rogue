b38cd514e28e0d506c5593fed7f42524942ea018
Geoff Driskell <geoff.driskell@gmail.com>
2022-05-27 10:22:45 -0400

    Created member variables

--- src/ECS/spriteComponent.hpp
+++ src/ECS/spriteComponent.hpp
@@ -12,7 +12,16 @@ private:
     SDL_Texture *texture;
     SDL_Rect srcRect, destRect;
 
+    bool animated = false;
+    int frames = 0;
+    int speed = 100;
+
 public:
+
+    int animIndex = 0;
+
+    std::map<const char*, Animation> animations;
+
     SpriteComponent() = default;
     SpriteComponent(const char* path)
     {
